be68eba576653074c6ed91464b8e2e026dc90949
Hugot FX <fxhugot@gmail.com>
2018-02-22 18:01:59 +0100

    comments

--- demodfm.py
+++ demodfm.py
@@ -3,12 +3,14 @@ import math
 
 ll = []
 
+# reading ASCII file containing IQ data
 filename=sys.argv[1]
 print("reading data from file "+filename)
 fd = open(filename, 'r')
 ll = fd.readlines()
 fd.close()
 
+# making a list containing IQ pairs
 c=0
 liq = []
 llen = len(ll)
@@ -32,6 +34,7 @@ def regul(dtheta):
 freq=96000
 dt=1./freq
 
+# real demodulation
 lv = []
 coeff = 0.05
 theta0=0.
@@ -40,10 +43,13 @@ mn=0
 for c in range(llen2):
   i = ll[2*c]
   q = ll[2*c+1]
+  # get atan(i/q)
   theta1 = math.atan2(float(q), float(i))
+  # calculates derivative
   dtheta = theta1 - theta0
   dtheta = regul(dtheta)
   v = coeff * dtheta / dt
+  # also calculates min and max for future renormalization purpose
   if v<mn: mn=v
   if v>mx: mx=v
   lv.append(v)
@@ -52,7 +58,7 @@ for c in range(llen2):
 print("mx "+str(mx))
 print("mn "+str(mn))
 
-# ascii dump
+# ascii dump of sound samples
 fd = open("demodfm.dat", 'w')
 for v in lv: fd.write(str(int(v))+"\n")
 fd.close()
